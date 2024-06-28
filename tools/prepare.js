e3aac6f2891f1b9ae8fb555fe884a471a3e1e7c8
ikrong <contact@ikrong.com>
2024-06-28 15:58:51 +0800

    修改vue脚本

---
+++ tools/prepare.js
@@ -0,0 +1,25 @@
+const core = require('@actions/core');
+const fetch = require('node-fetch');
+
+const args = process.argv.slice(2);
+
+async function prepareTagSources(repos) {
+    // git@github.com:owner/repo.git#revision
+    // https://api.github.com/repos/owner/repo/commits?per_page=1
+    const arr = repos.split(',').map(a=>a.trim()).filter(Boolean);
+    const result = await Promise.all(arr.map(async (repo) => {
+        const url = `https://api.github.com/repos/${repo}/commits?per_page=1`;
+        const res = await (await fetch(url)).json();
+        return `git@github.com:${repo}#${res[0].sha}`;
+    }));
+    core.exportVariable('DOCWORLD_SOURCE', result.join(','));
+}
+
+function main() {
+    const fn = {
+        'prepare-tag-source': prepareTagSources,
+    }
+    fn[args[0]](...args.slice(1));
+}
+
+main()
