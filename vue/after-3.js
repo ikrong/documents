7241dbff855e2f325785fc83c33ca7c20e65af92
ikrong <contact@ikrong.com>
2024-06-28 16:40:41 +0800

    写入sponsors

--- vue/after-3.js
+++ vue/after-3.js
@@ -6,9 +6,19 @@ const HOST_DIR = process.argv[2];
 
 fetch("https://sponsors.vuejs.org/data.json")
     .then((res) => res.text())
-    .then((text) => {
+    .then(async (text) => {
         const file = path.join(HOST_DIR, "sponsors.vuejs.org/data.json");
         const dir = path.dirname(file);
         fs.mkdirSync(dir, { recursive: true });
         fs.writeFileSync(file, text);
+        const data = JSON.parse(text);
+        const imgs = Object.values(data).flat().map(a=>a.img).filter(Boolean)
+        for(const img of imgs) {
+            const url = `https://sponsors.vuejs.org/images/${img}`;
+            const file = path.join(HOST_DIR, "sponsors.vuejs.org/images", img);
+            const dir = path.dirname(file);
+            fs.mkdirSync(dir, { recursive: true });
+            const buff = await fetch(url).then(res=>res.buffer());
+            fs.writeFileSync(file, buff);
+        }
     });
